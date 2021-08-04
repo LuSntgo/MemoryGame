be066e8829dc0c926b8a0560c290cefcef952701
LuSntgo <lu.sntgo@gmail.com>
2021-08-04 18:01:02 -0300

    on click the cards flip

--- Components/Card.js
+++ Components/Card.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState} from "react";
+import ReactCardFlip from "react-card-flip"; /*1*/
+import cardBack from "../images/CardBack.jpg";
 
 /* 1 */ 
 
-const Card = ({ card }) => {
+const Card = ({ card }) => { /* 3*/
+    const [flipped, changeFlip] = useState(false); /*1*/
+    /*3*/
+    const handleFlip =() => {
+        changeFlip(!flipped);;
+    }
     return (
         <div className="col-3 my-1">
             { /* 2 */ }
-            <img className ="mx-auto" src={card.front}
+            <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
+            <img className ="mx-auto" 
+            src={cardBack} {/*3*/}
+            
             height="100%"
             width="100%"
             key="front" 
+            onClick={() => handleFlip()} 
             />
+         <img className ="mx-auto" 
+            src={card.front} 
+            height="100%"
+            width="100%"
+            key="back" 
+            onClick={() => handleFlip()}  />
+            </ReactCardFlip>
         </div>
     );
 };
