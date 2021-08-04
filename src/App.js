025f12c480a8abf338d069ee73851a9a4422e3d7
LuSntgo <lu.sntgo@gmail.com>
2021-08-04 16:56:41 -0300

    Cards component refactor

--- src/App.js
+++ src/App.js
@@ -3,30 +3,25 @@ import "./App.css";
 
 //data
 import cards from "./data";
+//Components
+import Card from "./Components/Card"; /* 1 */
+
 //card back image
 import cardBack from "./images/CardBack.jpg";
 
 
 function App() {
+  let cardsGrid = cards.map(card => (
+    <Card key={card.id} card={card} />
+  )); /*2, 3 */
   return <div className="App border my-5">
     <div className="container">
-      <div className="row">
-        {/*1*/}
-        <div className="col-3 my-1">
-          {/*2*/}
-
-        <img className="mx-auto"
-        src={cardBack}
-        // used percentages instead of pixels to be responsive with the screen size
-        height="100%"
-        width="100%"
-        key="back" />
-        {/*3*/}
+      <div className="row"> {cardsGrid}</div>
+        
 
       </div>
     </div>
-    </div>
-  </div>;
+
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -53,3 +53,38 @@ const cards = [
 ];
 
 export default cards;
+
+import react from "react";
+import ".App.css";
+//data
+import cards from "./data"; /*1*/
+
+//card back image
+
+import cardBack from "../images/CardBack.jpg";
+function App(){
+  let cardsGrid = cards.map(card => {
+    return (
+      <div className="col-3 my-1">
+        <img className="mx-auto"
+        src={cardBack}
+        height = "100%"
+        width = "100%"
+        key="back"
+/>
+
+      </div>
+    )
+  })
+}
+/* 2 */
+return (
+  <div className="App border my-5">
+    <div className="container">
+      <div className="row">
+        {cardsGrid} 
+        {/* 3 */}
+      </div>
+    </div>
+  </div>
+)
